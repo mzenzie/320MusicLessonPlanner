45aea33c23d31ed424bf3962b2efc6b01d4e5890
mzenzie <festercube@gmail.com>
2015-02-28 13:41:25 -0500

    Begin Database Code
    Yes

---
+++ db.js
@@ -0,0 +1,23 @@
+function loadDB(path){
+
+    var fs = require('fs');
+    var SQL = require('sql.js');
+
+    var fileBuffer = fs.readFileSync(path);
+    var db = new SQL.Database(fileBuffer);
+    return db;
+}
+
+function saveDB(path){
+
+    var fs = require('fs');
+    var SQL = require('sql.js');
+
+    var data = db.export()
+    var buffer = newBuffer(data);
+    fs.writeFileSync(path, buffer);
+}
+
+function execute(db, command){
+    return db.exec(command);
+}

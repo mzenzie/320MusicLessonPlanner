761ec0924c358f55bc7181542bdb13ffe19ef27a
mzenzie <festercube@gmail.com>
2015-02-28 15:03:24 -0500

    Added More stuff

--- database/dbinit.js
+++ database/dbinit.js
@@ -2,5 +2,7 @@ var path = "./music_db.sqlite";
 var dblib = require('db.js');
 var manager = dblib.getDBManager();
 manager.db = manager.loadDB(path);
-manager.execute("CREATE TABLE TEACHER (TID PRIMARY KEY, EMAIL, CONTACT, ADDRESS, NAME)")
-manager.execute("CREATE TABLE STUDENT (SID PRIMARY KEY, NAME,
+manager.execute("CREATE TABLE TEACHER (TID PRIMARY KEY, EMAIL, CONTACT, ADDRESS, NAME)");
+manager.execute("CREATE TABLE STUDENT (SID PRIMARY KEY, TID FOREIGN KEY,  NAME, PHONE, EMAIL, ADDRESS, INSTRUMENT");
+manager.execute();
+manager.execute();

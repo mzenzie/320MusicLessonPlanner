e7cdc2b7757188a8e80bb99e42bf3bddf4db8392
xiuxiuzhu <xiuxiuzhu@umass.edu>
2015-03-04 14:25:21 -0500

    Update dbinit.js

--- database/dbinit.js
+++ database/dbinit.js
@@ -2,11 +2,11 @@ var fs = require("fs");
 var file = './mlp.sql'; //file used to store the data
 var exists = fs.existsSync(file);// if the file not exist create a new one
 
-teaTable = "CREATE TABLE Teacher(tid INTEGER PRIMARY KEY AUTOINCREMENT, Email TEXT, Name TEXT, Address TEXT, Phone TEXT);";
-//stuTable = "CREATE TABLE SRecord(sid INTEGER PRIMARY KEY AUTOINCREMENT, FName TEXT, LName TEXT, Instrument TEXT);";
-stuTable = "CREATE TABLE SRecord(sid INTEGER PRIMARY KEY, tid INTEGER references Teacher(tid), Email TEXT, FName TEXT, LName TEXT, Address TEXT, Phone TEXT, Instrument TEXT);";
-schTable = "CREATE TABLE Schedule(schid INTEGER PRIMARY KEY AUTOINCREMENT, Date DATE, Start time, End time, sid INTEGER references SRecord(sid));";
+teaTable = "CREATE TABLE Teacher(tid INTEGER PRIMARY KEY AUTOINCREMENT, Email TEXT, FName TEXT, LName TEXT, Address TEXT, Phone TEXT);";
+stuTable = "CREATE TABLE SRecord(sid INTEGER PRIMARY KEY, tid INTEGER references Teacher(tid), Email TEXT, FName TEXT, LName TEXT, Address TEXT, Phone TEXT, birthday DATE, Instrument TEXT, GeneralNotes TEXT);";
+schTable = "CREATE TABLE Schedule(schid INTEGER PRIMARY KEY AUTOINCREMENT, Date DATE, Start DATETIME, End DATETIME, sid INTEGER references SRecord(sid));";
 LRTable = "CREATE TABLE LessonRecord(lrid INTEGER PRIMARY KEY, Date DATE, Notes TEXT, sid INTEGER references SRecord(sid));";
+
 
 // connect to the database and return the pointer to db
 module.exports.init = function(){

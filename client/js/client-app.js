4a11d5186f9d3a8727cb5cc55d73217d4bb07e88
Jess Hendricks <jesshendricks1@Macbook-Pro.local>
2015-03-05 08:54:32 -0500

    Implemented $routeProvider for navigation. All page views are now called with AngularJS!!!

--- client/js/client-app.js
+++ client/js/client-app.js
@@ -1,1 +1,30 @@
-var appClient = angular.module("musicLessonPlanner", ['ngResource']);
+/**
+ * MusicLessonPlanner
+ * @author Team MLP
+ */
+
+/**
+ * Client AngularJS Web Application
+ */
+
+
+var appClient = angular.module("musicLessonPlanner", ['ngResource', 'ngRoute']);
+
+/**
+*	Configure the Routes
+*/
+
+appClient.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+		// Start Page
+		.when("/", {templateUrl: "/views/htmlViews/startPageView.html", controller: "pageController"})
+		// Student Record View
+		.when("/studentRecordView", {templateUrl: "/views/htmlViews/studentRecordPageView.html", controller: "pageController"})
+		// Lesson Notes View
+		.when("/lessonNoteView", {templateUrl: "/views/htmlViews/lessonNotePageView.html", controller: "pageController"});
+}]);
+
+
+appClient.controller('pageController', function (/* $scope, $location, $http */) {
+
+});

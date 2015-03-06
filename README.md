e9859a17a8ee8c3ac3b9b6ada70943a84af25f78
Jess Hendricks <jesshendricks1@Macbook-Pro.local>
2015-03-06 17:58:41 -0500

    More work on html and view controllers. Moved all view controller js in client-app.js Temporarily disabled the modal until I can figure out how to get it to work. Added animations and sample pages.

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
-Install node and express before running this app. 
-Might also have to run "npm install" before starting the server.
+# MusicLessonPlanner
 
-run "node app" in terminal to start server
-then access "localhost:8000" in the browser
+### Version
+1.0.0
+
+MusicLessonPlanner is an organizational application for private music teacher that keeps track of their studio of students, lesson notes, schedule, and progress.
+
+### Installation Instructions
+
+(These instructions will change once we are up and running with a URL, that way there is no need for the grader to install node)
+
+- Install node and express before running this app. 
+- You might also have to run "npm install" before starting the server.
+In the terminal run:
+```sh
+$ node app
+```
+Open your internet browser and go to the localhost:
+```sh
+localhost:8000
+```
+
+### Database Access Instructions
+
+TODO
+
+### Bug Tracking Instructions
+
+TODO
+
+### Tech
+
+MusicLessonPlanner is built using a derivative of the "MEAN" stack:
+* [AngularJS]: for enhanced HTML control
+* [Express]: framework for Node.JS
+* [node.js]: backend framework
+* [Twitter Bootstrap]: CSS styling framework
+* [SQLite3]: Database
+
+
+[node.js]:http://nodejs.org
+[Twitter Bootstrap]:http://twitter.github.com/bootstrap/
+[express]:http://expressjs.com
+[AngularJS]:http://angularjs.org
+[SQLite3]:https://sqlite.org
+
 
--- client/js/client-app.js
+++ client/js/client-app.js
@@ -8,23 +8,116 @@
  */
 
 
-var appClient = angular.module("musicLessonPlanner", ['ngResource', 'ngRoute']);
+ var appClient = angular.module("musicLessonPlanner", ['ngResource', 'ngRoute', 'ngAnimate']);
 
 /**
 *	Configure the Routes
 */
 
-appClient.config(['$routeProvider', function ($routeProvider) {
+appClient.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 	$routeProvider
 		// Start Page
-		.when("/", {templateUrl: "/views/htmlViews/startPageView.html", controller: "pageController"})
+		.when("/", {
+			templateUrl: "/views/htmlViews/startPageView.html", 
+			controller: "pageController"})
 		// Student Record View
-		.when("/studentRecordView", {templateUrl: "/views/htmlViews/studentRecordPageView.html", controller: "pageController"})
+		.when("/studentRecordView", {
+			templateUrl: "/views/htmlViews/studentRecordPageView.html", 
+			controller: "pageController"})
 		// Lesson Notes View
-		.when("/lessonNoteView", {templateUrl: "/views/htmlViews/lessonNotePageView.html", controller: "pageController"});
-}]);
+		.when("/lessonNoteView", {
+			templateUrl: "/views/htmlViews/lessonNotePageView.html", 
+			controller: "pageController"});
+	}]);
 
 
 appClient.controller('pageController', function (/* $scope, $location, $http */) {
 
 });
+
+
+appClient.controller('studentRecordController', ['$scope', '$resource', 
+	function ($scope, $resource){
+		var StudentRecord = $resource('/api/studentRecord');
+
+		StudentRecord.query(function (result) {
+			$scope.students = result;
+		});
+
+		$scope.students = [];
+		$scope.createStudentRecord = function () {
+			var newStudentRecord = new StudentRecord();
+			newStudentRecord.firstName = $scope.firstName;
+			newStudentRecord.lastName = $scope.lastName;
+			newStudentRecord.instrument = $scope.instrument;
+			newStudentRecord.email = $scope.email;
+			newStudentRecord.address = $scope.address;
+			newStudentRecord.startDate = $scope.startDate;
+			newStudentRecord.lessonTime = $scope.lessonTime;
+			newStudentRecord.$save(function (result){
+				StudentRecord.query(function (result){
+					$scope.students = result;
+				});
+				$scope.firstName = '';
+				$scope.lastName = '';
+				$scope.instrument = '';
+				$scope.email = '';
+				$scope.address = '';
+				$scope.startDate = '';
+				$scope.lessonTime = '';
+			});	
+
+			// $scope.showModal = false;
+
+			// $scope.toggleModal = function () {
+			// 	$scope.showModal = !$scope.showModal;
+			// };
+
+			//Closes the modal window after adding new student record
+
+			$('#addStudentForm').modal('hide');
+		}
+	}
+	]);
+
+// appClient.directive('modal', function (){
+// 	return {
+// 		template: '<div class="modal fade">' + 
+// 		'<div class="modal-dialog">' + 
+// 		'<div class="modal-content">' + 
+// 		'<div class="modal-header">' + 
+// 		'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+// 		'<h4 class="modal-title">{{ title }}</h4>' + 
+// 		'</div>' + 
+// 		'<div class="modal-body" ng-transclude></div>' + 
+// 		'</div>' + 
+// 		'</div>' + 
+// 		'</div>',
+// 		restrict: 'E',
+// 		transclude: true,
+// 		replace:true,
+// 		scope:true,
+// 		link: function postLink(scope, element, attrs) {
+// 			scope.title = attrs.title;
+
+// 			scope.$watch(attrs.visible, function(value){
+// 				if(value == true)
+// 					$(element).modal('show');
+// 				else
+// 					$(element).modal('hide');
+// 			});
+
+// 			$(element).on('shown.bs.modal', function(){
+// 				scope.$apply(function(){
+// 					scope.$parent[attrs.visible] = true;
+// 				});
+// 			});
+
+// 			$(element).on('hidden.bs.modal', function(){
+// 				scope.$apply(function(){
+// 					scope.$parent[attrs.visible] = false;
+// 				});
+// 			});
+// 		}
+// 	};
+// });

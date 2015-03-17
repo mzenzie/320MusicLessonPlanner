03514b21b42d884257420dbb48bd9449af5c9991
xiuxiuzhu <xiuxiuzhu@umass.edu>
2015-03-17 00:03:46 -0400

    Update dbinit.js

--- database/dbinit.js
+++ database/dbinit.js
@@ -2,12 +2,10 @@ var fs = require("fs");
 var file = './mlp.sql'; //file used to store the data
 var exists = fs.existsSync(file);// if the file not exist create a new one
 
-teaTable = "CREATE TABLE Teacher(tid INTEGER PRIMARY KEY AUTOINCREMENT, Email TEXT, FName TEXT, LName TEXT, Address TEXT, Phone TEXT);";
-//stuTable = "CREATE TABLE SRecord(sid INTEGER PRIMARY KEY AUTOINCREMENT, FName TEXT, LName TEXT, Instrument TEXT);";
-stuTable = "CREATE TABLE SRecord(sid INTEGER PRIMARY KEY AUTOINCREMENT, tid INTEGER references Teacher(tid), Email TEXT, FName TEXT, LName TEXT, Address TEXT, Phone TEXT, birthday DATE, Instrument TEXT, GeneralNotes TEXT);";
-schTable = "CREATE TABLE Schedule(schid INTEGER PRIMARY KEY AUTOINCREMENT, Date DATE, Start DATETIME, End DATETIME, sid INTEGER references SRecord(sid));";
-LRTable = "CREATE TABLE LessonRecord(lrid INTEGER PRIMARY KEY AUTOINCREMENT, Date DATE, Notes TEXT, sid INTEGER references SRecord(sid));";
-
+teaTable = "CREATE TABLE Teacher(tEmail TEXT PRIMARY KEY, passowrd TEXT, firstName TEXT, lastName TEXT, address TEXT, phone TEXT);";
+stuTable = "CREATE TABLE SRecord(tEmail TEXT references Teacher(tEmailmail) on delete cascade on update cascade, email TEXT, firstName TEXT, lastName TEXT, address TEXT, phone TEXT, birthday DATE, instrument TEXT, generalNotes TEXT, PRIMARY KEY (email, instrument));";
+schTable = "CREATE TABLE Schedule(lsid INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, lessonTime DATETIME, lessonLength INTEGER, email TEXT references SRecord(email) on delete cascade on update cascade, instrument TEXT references SRecord(instrument) on delete cascade on update cascade);";
+LRTable = "CREATE TABLE LessonRecord(lrid INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, notes TEXT, sid INTEGER references SRecord(sid), email TEXT references SRecord(email) on delete cascade on update cascade, instrument TEXT references SRecord(instrument) on delete cascade on update cascade);";
 // connect to the database and return the pointer to db
 var db = null;
 

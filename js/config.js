83b2cc1f9abe7d982d314ffff385e20e04f800a3
Jess Hendricks <jesshendricks1@Macbook-Pro.local>
2015-03-23 10:11:20 -0400

    Test edit

--- js/config.js
+++ js/config.js
@@ -1,16 +1,16 @@
 /**
-* INSPINIA - Responsive Admin Theme
-* Copyright 2015 Webapplayers.com
-*
-* Inspinia theme use AngularUI Router to manage routing and views
-* Each view are defined as state.
-* Initial there are written state for all view in theme.
-*
-*/
+ * INSPINIA - Responsive Admin Theme
+ * Copyright 2015 Webapplayers.com
+ *
+ * Inspinia theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
 function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, jwtInterceptorProvider, $httpProvider) {
     $urlRouterProvider.otherwise("/index/main");
 
-    jwtInterceptorProvider.tokenGetter = function(store){
+    jwtInterceptorProvider.tokenGetter = function(store) {
         return store.get('token'); //storage field = 'token'
     }
     $httpProvider.interceptors.push('jwtInterceptor');
@@ -21,39 +21,48 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, jwtInte
     });
 
     $stateProvider
-    
-    .state('index', {
-        abstract: true,
-        url: "/index",
-        controller: 'signoutController',
-        templateUrl: "views/common/content_top_navigation.html",
-    })
-    .state('index.main', {
-        url: "/main",
-        templateUrl: "views/startPageView.html",
-        controller: studentRecordController,
-        data: { pageTitle: 'Main Page', requiresLogin: true}
-    })
-    .state('index.studentRecordViewPage', {
-        url: "/studentRecordPageView",
-        templateUrl: "views/studentRecordPageView.html",
-        controller: studentRecordController,
-        data: { pageTitle: 'Student Records', requiresLogin: true}
-    })
-    .state('index.lessonNoteViewPage', {
-        url: "/lessonNotePageView",
-        templateUrl: "views/lessonNotePageView.html",
-        data: { pageTitle: 'Lesson Notes', requiresLogin: true }
-    })
-    .state('signin', {
-        url: '/signin',
-        controller: 'signinController',
-        templateUrl: 'login.html'
-    })
-    // .state('index.signout', {
-    //     url: '/index',
-    //     template: 'views/common/top_navigation.html',
-        
+
+        .state('index', {
+            abstract: true,
+            url: "/index",
+            controller: 'signoutController',
+            templateUrl: "views/common/content_top_navigation.html",
+        })
+        .state('index.main', {
+            url: "/main",
+            templateUrl: "views/startPageView.html",
+            controller: studentRecordController,
+            data: {
+                pageTitle: 'Main Page',
+                requiresLogin: true
+            }
+        })
+        .state('index.studentRecordViewPage', {
+            url: "/studentRecordPageView",
+            templateUrl: "views/studentRecordPageView.html",
+            controller: studentRecordController,
+            data: {
+                pageTitle: 'Student Records',
+                requiresLogin: true
+            }
+        })
+        .state('index.lessonNoteViewPage', {
+            url: "/lessonNotePageView",
+            templateUrl: "views/lessonNotePageView.html",
+            data: {
+                pageTitle: 'Lesson Notes',
+                requiresLogin: true
+            }
+        })
+        .state('signin', {
+            url: '/signin',
+            controller: 'signinController',
+            templateUrl: 'login.html'
+        })
+        // .state('index.signout', {
+        //     url: '/index',
+        //     template: 'views/common/top_navigation.html',
+
     //     data: {requiresLogin : true}
     // })
     .state('signup', {
@@ -64,22 +73,22 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, jwtInte
 
 }
 angular
-.module('inspinia')
-.config(config)
-.run(function($rootScope, $state, store, jwtHelper, $location) {
-    $rootScope.$state = $state;
-    $rootScope.$on('$stateChangeStart', function(e, toState){
-        if (toState.data && toState.data.requiresLogin){
-            if (!store.get('token')){
-                e.preventDefault();
-                $state.go('signin');
+    .module('inspinia')
+    .config(config)
+    .run(function($rootScope, $state, store, jwtHelper, $location) {
+        $rootScope.$state = $state;
+        $rootScope.$on('$stateChangeStart', function(e, toState) {
+            if (toState.data && toState.data.requiresLogin) {
+                if (!store.get('token')) {
+                    e.preventDefault();
+                    $state.go('signin');
+                }
             }
-        }
+        });
+
+    })
+    .controller('AppController', function($scope, $location) {
+        $scope.$on('$stateChangeSuccess', function(e, nextRoute) {
+
+        });
     });
-   
-})
-.controller('AppController', function ($scope, $location) {
-    $scope.$on('$stateChangeSuccess', function(e, nextRoute){
-        
-    });
-});

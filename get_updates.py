ebe402307545ced7f2d714280f52f650d68031cc
mzenzie <festercube@gmail.com>
2015-03-28 16:09:01 -0400

    Updated script to use simple telnet interface

--- get_updates.py
+++ get_updates.py
@@ -4,21 +4,30 @@
 from twisted.internet import protocol, reactor, endpoints
 import subprocess
 
-received = []
+END_CMD = ["pkill","-f","node"]
+RUN_CMD = ["runapp"]
+UPDATE_CMD = ["update"]
 
-PORT = 4774
+UPDATE_PORT = 4774
+RUN_PORT = 5885
 
 class Update(protocol.Protocol):
-    def dataReceived(self,data):
-        global received
-        received.append(data)
-        if("u" in data):
-            received = []
-            subprocess.Popen(["git","pull"], shell=True)
+    def connectionMade(self):
+        subprocess.Popen(UPDATE_CMD)
 
 class UpdateFactory(protocol.Factory):
     def buildProtocol(self, addr):
         return Update()
 
-endpoints.serverFromString(reactor, "tcp:"+str(PORT)).listen(UpdateFactory())
+class Run(protocol.Protocol):
+    def connectionMade(self):
+        subprocess.Popen(END_CMD)
+        subprocess.Popen(RUN_CMD)
+
+class RunFactory(protocol.Protocol):
+    def buildProtocol(self, addr):
+        return Run()
+
+endpoints.serverFromString(reactor, "tcp:"+str(UPDATE_PORT)).listen(UpdateFactory())
+endpoints.serverFromString(reactor, "tcp:"+str(RUN_PORT)).listen(RunFactory())
 reactor.run()

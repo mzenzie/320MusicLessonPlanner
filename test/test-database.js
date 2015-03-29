10cf4fb9019f7ad8682017a901622b6b7fd6ba78
David-Carlson <britishorbust@gmail.com>
2015-03-29 17:52:56 -0400

    Database tests are working

--- test/test-database.js
+++ test/test-database.js
@@ -1,12 +1,11 @@
 var assert = require('assert');
-var app = require('../database/dbinit.js'); // the module
+
 var fs = require('fs');
 var format = require('string-format');
-
 describe('Database', function() {
-    var db;
+    var db, app;
     var student = {};
-    var student2 = {};
+    var self;
     var d = new Date();
     var n = d.toDateString();
     
@@ -23,7 +22,7 @@ describe('Database', function() {
         lessonTime: '12:10',
         lessonLength: '2:00',
     }
-    student2.body = {
+    self = {
         firstName: 'Kyle',
         lastName: 'Lemmings',
         instrument: 'Timpani',
@@ -37,50 +36,77 @@ describe('Database', function() {
         lessonLength: '2:00',
     }
 
-    beforeEach(function () {
-        app.init();
-        app.reinit();
+    function setup()
+    {
+        app = require('../database/dbinit.js');
+        app.init();        
         db = app.getInstance();
-    });
+
+        console.log("before each");
+        console.log(db==undefined);
+    }
 
     describe('database initialized', function(){
+        setup()
         var exists = fs.existsSync('./mlp.sql');
         it('mlp.sql created', function(){
             assert.equal(true, exists);
-        });   
+        })  
         it('db not null', function()
         {
             assert.notEqual(db, null);
-        });
+        })
+        it('db not undefined', function()
+        {
+            assert.notEqual((db==undefined), true)
+        })
         it('db has run function', function(){
             assert.equal(typeof db.run, 'function');
-        });
-    });
+        })
+    })
 
     
-    describe('Gets instance correctly', function() {
-        
-        it('Create returns given record', function() {
-            
+    describe('Adds to database', function() {   
+        setup();
+        var student_record_query = 
+            format("INSERT INTO SRecord (tid, firstName, lastName, email, address, phone, birthday, instrument) VALUES({0}, '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}')",
+                1,
+                self.firstName,
+                self.lastName,
+                self.email,
+                self.address,
+                self.phone,
+                self.birthday,
+                self.instrument); 
+     
+        db.get(student_record_query, function(err, row){
+            it('returns given record', function() {
+                assert.notEqual(row, null);               
+            });
+            it('returns without error', function(){
+                  assert.equal(err, null);
+            assert.equal(row.sid, self.sid);
+            });
         });
-    });
+    })
+
 
 
     describe('getInstance', function () {
         console.log()
         it('function exists', function () {
             assert.equal(typeof app.getInstance, 'function');
-        });
-    });
+        })
+    })
     describe('reinit', function () {
         it('function exists', function () {
             assert.equal(typeof app.reinit, 'function');
-        });
-    });
+        })
+    })
     describe('init', function () {
         it('function exists', function () {
             assert.equal(typeof app.init, 'function');
-        });
-    });
+        })
+    })
 
 });

a45fd2bf599981dd806ad25bd7ae454ae757445c
David-Carlson <britishorbust@gmail.com>
2015-03-30 22:02:23 -0400

    Added protractor test

---
+++ test/Protractor_Tests/loginSpec.js
@@ -0,0 +1,28 @@
+// File: chapter14/simpleRoutingSpec.js
+
+describe('Routing Test', function() {
+
+    browser.go('/');
+    browser.debugger();
+    /*
+        var ptor, driver;
+     describe('Editors', function() {
+        ptor = protractor.getInstance();
+        driver = ptor.driver;
+        it('Can log in', function() {
+            driver.get('http://localhost:8000/');
+            browser.debugger();
+            //findByName('login').sendKeys('test_editor');
+            //findByName('pass').sendKeys('rubbish');
+            //driver.findElement(protractor.By.css('button[type="submit"]')).click();
+        }, 20000);
+
+        /*
+        it('Shows the homepage', function() {
+            var el = ptor.findElement(protractor.By.css('h1[field="pageTitle"]'));
+            expect(el.getText()).toEqual('Welcome to Home');
+        }, 30000);
+
+    });*/
+
+});

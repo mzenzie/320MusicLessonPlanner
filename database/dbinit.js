9f25d3714873fce793d15d903fab6ae3e95ceffc
Xiu Zhu <xiuxiuzhu78@umassguest150.sys.cs.local>
2015-04-08 13:51:34 -0400

    update database

--- database/dbinit.js
+++ database/dbinit.js
@@ -5,14 +5,14 @@ var exists = fs.existsSync(file); // if the file not exist create a new one
 accTable = "CREATE TABLE Account(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT);";
 teaTable = "CREATE TABLE Teacher(tid INTEGER PRIMARY KEY AUTOINCREMENT, id INTEGER references Account(id) on delete cascade on update cascade, email TEXT, firstName TEXT, lastName TEXT, address TEXT, phone TEXT);";
 stuTable = "CREATE TABLE SRecord(sid INTEGER PRIMARY KEY AUTOINCREMENT, tid INTEGER references Teacher(tid) on delete cascade on update cascade, email TEXT, firstName TEXT, lastName TEXT, address TEXT, phone TEXT, birthday DATE, instrument TEXT, generalNotes TEXT);";
-schTable = "CREATE TABLE Schedule(lsid INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, lessonTime DATETIME, lessonLength INTEGER, sid INTEGER references SRecord(sid) on delete cascade on update cascade);";
-LRTable = "CREATE TABLE LessonRecord(lrid INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, notes TEXT, sid TEXT references SRecord(sid) on delete cascade on update cascade);";
+schTable = "CREATE TABLE Schedule(lsid INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, lessonTime DATETIME, lessonLength INTEGER, notes TEXT, sid INTEGER references SRecord(sid) on delete cascade on update cascade);";
+//LRTable = "CREATE TABLE LessonRecord(lrid INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, notes TEXT, sid TEXT references SRecord(sid) on delete cascade on update cascade);";
 
 daccTable = "DROP TABLE IF EXISTS Account";
 dteaTable = "DROP TABLE IF EXISTS Teacher;"
 dstuTable = "DROP TABLE IF EXISTS SRecord;"
 dschTable = "DROP TABLE IF EXISTS Schedule;"
-dLRTable = "DROP TABLE IF EXISTS LessonRecord;"
+//dLRTable = "DROP TABLE IF EXISTS LessonRecord;"
 
 // connect to the database and return the pointer to db
 var db = null;
@@ -29,7 +29,7 @@ module.exports.init = function() {
             db.run(teaTable);
             db.run(stuTable);
             db.run(schTable);
-            db.run(LRTable);
+            //db.run(LRTable);
             /*var stmt = db.prepare("INSERT INTO SRecord VALUES(?, ?, ?, ?)");
 			stmt.run(1, "Jack", "Benny", "Piano");
 			stmt.run(2, "Bruce", "Springsteen", "Violin");
@@ -51,32 +51,32 @@ module.exports.reinit = function() {
     var sqlite3 = require("sqlite3").verbose();
     db = new sqlite3.Database(file);
     db.serialize(function() {
-        db.run(daccTable);
         db.run(dschTable);
-        db.run(dLRTable);
+        //db.run(dLRTable);
         db.run(dstuTable);
         db.run(dteaTable);
+        db.run(daccTable);
     });
 
 // <<<<<<< HEAD
-	db.serialize(function(){
-		db.run(teaTable);
-		db.run(stuTable);
-		db.run(schTable);
-		db.run(LRTable);
-		db.run(daccTable);
-		db.run("PRAGMA foreign_keys = ON");
+// 	db.serialize(function(){
+// 		db.run(teaTable);
+// 		db.run(stuTable);
+// 		db.run(schTable);
+// 		//db.run(LRTable);
+// 		db.run(daccTable);
+// 		db.run("PRAGMA foreign_keys = ON");
 
-});
+// });
 // =======
-//     db.serialize(function() {
-//         db.run(daccTable);
-//         db.run(teaTable);
-//         db.run(stuTable);
-//         db.run(schTable);
-//         db.run(LRTable);
-//         db.run("PRAGMA foreign_keys = ON");
+    db.serialize(function() {
+        db.run(accTable);
+        db.run(teaTable);
+        db.run(stuTable);
+        db.run(schTable);
+        //db.run(LRTable);
+        //db.run("PRAGMA foreign_keys = ON");
 
-//     });
+    });
 // >>>>>>> UIDeveloperBranch
 }

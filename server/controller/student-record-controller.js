b570ccfd518ffc42725991adf2dad892a98cb78a
Jess Hendricks <jesshendricks1@Macbook-Pro.local>
2015-04-11 08:41:36 -0400

    Another major refactoring of controllers.js to simplify the scope of $scope.
    Issue: Today view does not update if a new student is create having a lesson today.

--- server/controller/student-record-controller.js
+++ server/controller/student-record-controller.js
@@ -7,31 +7,29 @@ module.exports.create = function(req, res) {
     // > POST /api/studentRecord
 
     /* triggered for every GET /api/student-record.
-		could've just passed req.body to create, but test to see first.
-		note** req -> Request, res -> Response
-	  */
+        could've just passed req.body to create, but test to see first.
+        note** req -> Request, res -> Response
+      */
     console.log("CTRLLER");
     console.log(req.body);
-    var tid = Account.getIDFromToken(req.headers.authorization); 
-    if (tid==null) tid = 1; // FOR TESTING 
+    var tid = Account.getIDFromToken(req.headers.authorization);
+    if (tid == null) tid = 1; // FOR TESTING 
     console.log("TID FOR INSERTION ======================== " + tid);
-    StudentRecord.create(   
-        {
-            tid       : tid,
-            firstName : req.body.firstName,
-            lastName  : req.body.lastName,
-            email     : req.body.email,
-            instrument: req.body.instrument,
-            address   : req.body.address,
-            phone     : req.body.phone,
-            birthday  : req.body.birthday,
-            startDate : req.body.startDate,
-            lessonTime: req.body.lessonTime,
-            lessonLength: req.body.lessonLength,
-            numberOfLessons: req.body.numberOfLessons,
-            generalNotes   : req.body.generalNotes    
-        }
-    , function(err, newStudentRecord) {
+    StudentRecord.create({
+        tid: tid,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        instrument: req.body.instrument,
+        address: req.body.address,
+        phone: req.body.phone,
+        birthday: req.body.birthday,
+        startDate: req.body.startDate,
+        lessonTime: req.body.lessonTime,
+        lessonLength: req.body.lessonLength,
+        numberOfLessons: req.body.numberOfLessons,
+        generalNotes: req.body.generalNotes
+    }, function(err, newStudentRecord) {
         if (err != null) {
             res.json({});
         } else {
@@ -48,11 +46,11 @@ module.exports.get = function(req, res) {
     // var sess = req.session;
     // var id = sess.id; // to be implemented...
     var sid = req.query.id;
-    var tid = Account.getIDFromToken(req.headers.authorization); 
-    if (tid==null) tid = 1;
-    if (sid === undefined){
+    var tid = Account.getIDFromToken(req.headers.authorization);
+    if (tid == null) tid = 1;
+    if (sid === undefined) {
         //list
-        console.log("TID+======"+tid);
+        console.log("TID+======" + tid);
         StudentRecord.list(tid, function(err, studentRecords) {
             if (err != null) {
                 res.status(400).send("error listing studentRecords");
@@ -61,15 +59,15 @@ module.exports.get = function(req, res) {
             }
         });
     } else {
-        StudentRecord.get(sid, function(err, studentRecord){
-            if (err!=null || studentRecord==null){
+        StudentRecord.get(sid, function(err, studentRecord) {
+            if (err != null || studentRecord == null) {
                 res.status(400).send("couldn't find StudentRecord") // bad request
             } else {
                 console.log("Retrieving studentRecord's lesson schedule");
                 res.json(studentRecord);
             }
         })
-        
+
     }
 };
 
@@ -77,7 +75,7 @@ module.exports.get = function(req, res) {
 module.exports.delete = function(req, res) {
     // > DELETE /api/studentRecord/
     var sid = req.query.id;
-    if (sid === undefined){
+    if (sid === undefined) {
         res.status(400).send("invalid sid");
     } else {
         StudentRecord.delete(sid, function(err) {
@@ -99,23 +97,22 @@ NEED TO RECODE PRIMARY KEY FROM EMAIL, INSTRUMENT to
 */
 
 module.exports.update = function(req, res) {
-    // > PUT /api/studentRecord/:id
-    if(req.query.sid === undefined){
+    // > PUT /api/studentRecord/
+    if (req.query.sid === undefined) {
         res.status(400).send("invalid sid");
-    }else{
-        StudentRecord.get(req.query.sid, function(err, studentRecord){
-            if(studentRecord == null){
+    } else {
+        StudentRecord.get(req.query.sid, function(err, studentRecord) {
+            if (studentRecord == null) {
                 res.send("Invalid sid");
-            }else{
-            
-                 for(var key in req.body){
-                    if(req.body.hasOwnProperty(key)){
+            } else {
+                for (var key in req.body) {
+                    if (req.body.hasOwnProperty(key)) {
                         var val = req.body[key];
                         studentRecord[key] = val;
                     }
                 }
-                studentRecord.update(function(err, studentRecord){
-                    if (err!=null){
+                studentRecord.update(function(err, studentRecord) {
+                    if (err != null) {
                         res.status(400).send("unable to update StudentRecord");
                     } else {
                         res.status(200).send(studentRecord);
@@ -125,7 +122,7 @@ module.exports.update = function(req, res) {
         });
     }
 
-//email TEXT, firstName TEXT, lastName TEXT, address TEXT, phone TEXT, birthday DATE, instrument TEXT, generalNotes 
+    //email TEXT, firstName TEXT, lastName TEXT, address TEXT, phone TEXT, birthday DATE, instrument TEXT, generalNotes 
     /*
     if (req.query.email === undefined || req.query.instrument === undefined){
         req.status(400).send("invalid email, instrument");

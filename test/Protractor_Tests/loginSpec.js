d9e1f5ec30114bf881f88349f03e3b1acce310e6
David-Carlson <britishorbust@gmail.com>
2015-04-13 00:54:44 -0400

    Fixing integration and unit tests

--- test/Protractor_Tests/loginSpec.js
+++ test/Protractor_Tests/loginSpec.js
@@ -1,70 +1,61 @@
-// File: chapter14/simpleRoutingSpec.js
-browser.getCapabilities().then(function (cap) {
-  browser.browserName = cap.caps_.browserName;
-});
+describe('Routing validation', function(){
 
-function getCorrectUrl(url){
-  if(browser.browserName != 'firefox')
-    return '?' + url;
-}
+  describe('Pre-login functionality', function() {
+    it('should reach Main, About and support screen', function(){
+      browser.get('#/').then(function(){ 
+        expect(browser.getCurrentUrl()).toMatch('/#/startpage/landing')});
+      browser.get('/#/startpage/about').then(function(){ 
+        expect(browser.getCurrentUrl()).toMatch('/#/startpage/about')});
+      browser.get('/#/startpage/support').then(function(){ 
+        expect(browser.getCurrentUrl()).toMatch('/#/startpage/support')});
+      browser.get('#/').then(function(){ 
+        expect(browser.getCurrentUrl()).toMatch('/#/startpage/landing')});
+   });
 
-describe('Routing Test', function() {
-
-  describe('Should get to login page and login given the correct info', function() {
-    // Open the landing page
-    //console.log(browser.browserName + ' is my name');
-    browser.get('#/');
-      // Ensure that the user was redirected
-    expect(browser.getCurrentUrl()).toEqual('http://localhost:8000/#/startpage/landing');
-
-    it('should navigate to support and about', function(){
-      browser.get('/#/startpage/about');
-      expect(browser.getCurrentUrl()).toEqual('/#/startpage/about');
-      browser.get('#/startpage/support');
-      expect(browser.getCurrentUrl()).toEqual('/#/startpage/support');
-      browser.get('#/');
+    it('should login', function(){
+      var username = element(by.model('username'));
+      var password = element(by.model('password'));
+      username.sendKeys('admin@g.com');
+      password.sendKeys('1234');
+      element(by.buttonText('Login')).click()
+        .then(function(){ expect(browser.getCurrentUrl()).toMatch('/#/teacher-dashboard/main')});
+      //browser.debugger();
     });
-
-    var username = element(by.model('username'));
-    var password = element(by.model('password'));
-    username.sendKeys('admin@g.com');
-    password.sendKeys('1234');
-    element(by.buttonText('Login')).click();
-     var username = element(by.model('username'));
-    var password = element(by.model('password'));
-    username.sendKeys('admin@g.com');
-    password.sendKeys('1234');
-    element(by.buttonText('Login')).click();
-    
-
   });
 
+  describe('Landing page functionality', function(){
+    it('should create a student record', function(){
+      element(by.buttonText('Add New Student')).click();
+      //browser.debugger();
+      element(by.model('firstName')).sendKeys('testFirstName');
+      element(by.model('lastName')).sendKeys('testLastName');
+      element(by.model('instrument')).sendKeys('my instrument');
+      element(by.model('email')).sendKeys('dscarlso@umass.edu');
+      element(by.model('phone')).clear().then(function(){element(by.model('phone')).sendKeys('1234567890');});
+      element(by.model('address')).sendKeys('my address');
+      element(by.model('birthday')).sendKeys('1/1/2000');
+      element(by.model('startDate')).sendKeys('1/1/2000');
+      element(by.model('numberOfLessons')).sendKeys('10');
+       //browser.debugger();
+      element(by.css('[ng-click="ok()"]')).click();
+      browser.debugger();
+    });
+  });
+
+  
+});
+/*
+describe('Routing Test', function() {
+
   it('Should create a new student', function(){
-    browser.debugger();
+    
     //expect(browser.getCurrentUrl()).toEqual('/#/teacher-dashboard/main');
     //browser.debugger();
-    //element(by.buttonText('Add New Student')).click();
-    //browser.debugger();
-    /*
-    element(by.css('[ng-click="createStudentRecord()"]')).click();
-    element(by.model('firstName')).sendKeys('my firstName');
-    element(by.model('lastName')).sendKeys('my lastName');
-    element(by.model('instrument')).sendKeys('my instrument');
-    element(by.model('email')).sendKeys('my email');
-    element(by.model('phone')).sendKeys('my phone');
-    element(by.model('address')).sendKeys('my address');
-    element(by.model('birthday')).sendKeys('1/1/2000');
-    element(by.model('startDate')).sendKeys('1/1/2000');
-    element(by.model('numberOfLessons')).sendKeys('10');
-    element(by.css('[ng-click="ok()"]')).click();
-    */
+
+    
   })
 
   it('Should correctly display new student', function(){
     browser.debugger();
   })
-
-
-
-
-});
+});*/
